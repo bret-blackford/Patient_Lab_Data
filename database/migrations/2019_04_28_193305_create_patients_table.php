61237fb402fbcc68ce433c041bda8ad77a1c5db0
Bret Blackford <bret_blackford@yahoo.com>
2019-04-28 14:49:09 -0500

    addedd seeders to my PATIENTS and LABS tables

---
+++ database/migrations/2019_04_28_193305_create_patients_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePatientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('patients', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            
+            $table->string('last_name');
+            $table->string('first_name');
+            $table->dateTime('bithdate');
+            $table->enum('gender', ['male', 'female']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('patients');
+    }
+}
---
+++ database/seeds/PatientsTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Patient;
+
+class PatientsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+       $patientArray = [
+           ['Blackford', 'Bret','1964-07-16','Male'],
+            ['Blackford', 'Earl', '1937-01-01', 'Male'],
+            ['Mouse', 'Minnie', '1931-01-30','Female'],
+        ];
+        
+        $count = count($patientArray);
+        
+        foreach($patientArray as $key=>$patientData){
+            $patient = new Patient();
+            
+            //$patient->created_at = Carbon\Carbon::now()->subDays($count)->toDateTimeString();
+            $patient->last_name = $patientData[0];
+            $patient->first_name = $patientData[1];
+            $patient->bithdate = $patientData[2];
+            $patient->gender = $patientData[3];
+            
+            $patient->save();
+            $count--;
+        }
+    }
+}
